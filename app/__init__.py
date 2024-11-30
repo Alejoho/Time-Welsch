8972288ce4ec101eb200b2a7a31742a5be1ddb97
Alejoho <alejoreilly96@gmail.com>
2024-11-30 18:05:41 -0500

    The confirmation token works correct

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,18 @@
 from flask import Flask
+from itsdangerous import URLSafeTimedSerializer
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
+# TODO: Ask copilot if i can do with the serializer what I do with the sqlachemy, migrate, and flask_login packages
 
 
 def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = "secret"
     app.config["HUNTER_API_KEY"] = os.getenv("HUNTER_API_KEY")
+    serializer = URLSafeTimedSerializer(app.config["SECRET_KEY"])
+    app.config["SERIALIZER"] = serializer
 
     from app.routes import index_routes_bp
 
--- app/routes/index.py
+++ app/routes/index.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, current_app
 from app.forms import LoginForm
 
 bp = Blueprint("index_routes", __name__)
@@ -8,9 +8,28 @@ bp = Blueprint("index_routes", __name__)
 def index():
     form = LoginForm()
     username = None
+    token = None
 
     if form.validate_on_submit():
         username = form.username.data
+        email = form.email.data
         form.username.data = ""
+        form.email.data = ""
 
-    return render_template("index.html", form=form, username=username)
+        serializer = current_app.config["SERIALIZER"]
+
+        token = serializer.dumps(email, "email_confirmation")
+
+    return render_template("index.html", form=form, username=username, token=token)
+
+
+@bp.get("/confirm_email/<token>")
+def confirm_email(token):
+    serializer = current_app.config["SERIALIZER"]
+
+    try:
+        email = serializer.loads(token, salt="email_confirmation", max_age=10)
+    except:
+        return "Something went wrong with the confirmation try again"
+
+    return "The email works"
