29860f40c6acef3f4e8a4e73f02b141fddba5c63
Alejoho <alejoreilly96@gmail.com>
2024-12-04 12:17:20 -0500

    Add the content of the about the author page

--- app/routes/home_routes.py
+++ app/routes/home_routes.py
@@ -6,3 +6,8 @@ bp = Blueprint("home_routes", __name__)
 @bp.get("/")
 def home():
     return render_template("test.html")
+
+
+@bp.get("/sobre_el_autor")
+def about_author():
+    return render_template("author.html")
