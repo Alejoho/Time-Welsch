477d60abdf60d1b58c90f12888051a83c87fd06e
Alejoho <alejoreilly96@gmail.com>
2024-12-06 19:56:05 -0500

    Add some dependencies

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,16 @@ from flask import Flask
 from dotenv import load_dotenv
 import os
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
+from flask_migrate import Migrate
+from flask_login import LoginManager
 
 load_dotenv()
 
 db = SQLAlchemy()
+csrf = CSRFProtect()
+migrate = Migrate()
+login_manager = LoginManager()
 
 
 def create_app():
@@ -18,6 +24,10 @@ def create_app():
     app.config["RECAPTCHA_PRIVATE_KEY"] = os.getenv("RECAPTCHA_PRIVATE_KEY")
 
     db.init_app(app)
+    csrf.init_app(app)
+    migrate.init_app(app, db)
+    # login_manager.init_app(app)
+    # login_manager.login_view = "home_routes.login"
 
     from app.routes import home_routes_bp
 
