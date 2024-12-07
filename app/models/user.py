ac53a6ed760a6d7ce013c8704dd267d0bc04d288
Alejoho <alejoreilly96@gmail.com>
2024-12-06 19:58:21 -0500

    Create the user table in the db

--- app/models/user.py
+++ app/models/user.py
@@ -7,6 +7,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class User(db.Model):
+    __tablename__ = "users"
     id: Mapped[int] = mapped_column(primary_key=True)
     username: Mapped[str] = mapped_column(String(50), unique=True)
     email: Mapped[str] = mapped_column(String(100), unique=True)
