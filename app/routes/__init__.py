bef1849adb0ede390b4362140fc9b3d904fd69e0
Alejoho <alejoreilly96@gmail.com>
2024-12-10 21:35:30 -0500

    Extract the register routes to an independent file

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -2,3 +2,4 @@ from app.routes.home_routes import bp as home_routes_bp
 from app.routes.error_routes import bp as error_routes_bp
 from app.routes.main_routes import bp as main_routes_bp
 from app.routes.login_routes import bp as login_routes_bp
+from app.routes.register_routes import bp as register_routes_bp
