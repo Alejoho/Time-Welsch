87e00df7b7a852024c73d57468d89c1a39b519cf
Alejoho <alejoreilly96@gmail.com>
2024-12-13 04:04:19 -0500

    Add the chapters table to the db

---
+++ app/models/chapter.py
@@ -0,0 +1,10 @@
+from app import db
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String
+
+
+class Chapter(db.Model):
+    __tablename__ = "chapters"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    number: Mapped[int] = mapped_column(unique=True)
+    name: Mapped[str] = mapped_column(String(100), unique=True)
