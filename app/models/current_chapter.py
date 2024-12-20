9de31ff6e0c85515d40f183f528eddec650dfab7
Alejoho <alejoreilly96@gmail.com>
2024-12-20 09:35:24 -0500

    Make changes to the current_chapters table

--- app/models/current_chapter.py
+++ app/models/current_chapter.py
@@ -1,21 +1,16 @@
 from app import db
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import (
-    UniqueConstraint,
-    ForeignKeyConstraint,
-)
+from sqlalchemy import UniqueConstraint, ForeignKeyConstraint, PrimaryKeyConstraint
 from app.models import User
 
 
 class CurrentChapter(db.Model):
     __tablename__ = "current_chapters"
-    # LATER: Remove this id. And create a compound primary key with the other 2 columns
-    id: Mapped[int] = mapped_column(primary_key=True)
     user_id: Mapped[int]
     current_chapter: Mapped[int] = mapped_column(default=1)
 
     __table_args__ = (
-        # LATER: Match the naming of the completed_chapters table. (UQ_current_chapters_user_id)
+        PrimaryKeyConstraint("user_id", "current_chapter", name="PK_current_chapters"),
         UniqueConstraint("user_id", name="UQ_user_id"),
         ForeignKeyConstraint(
             ["user_id"], ["users.id"], name="FK_user_id", ondelete="CASCADE"
