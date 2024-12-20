2d640e11404be2ff6c0f87f2936525fb1b858dfb
Alejoho <alejoreilly96@gmail.com>
2024-12-20 09:43:05 -0500

    Name the indexes of the chapters table

--- app/models/chapter.py
+++ app/models/chapter.py
@@ -1,15 +1,21 @@
 from app import db
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String
+from sqlalchemy import String, UniqueConstraint
 
 
 class Chapter(db.Model):
     __tablename__ = "chapters"
+
     # LATER: remove the column id and set the number as primary key but without autoincrementing
     id: Mapped[int] = mapped_column(primary_key=True)
-    number: Mapped[int] = mapped_column(unique=True)
-    name: Mapped[str] = mapped_column(String(100), unique=True)
+    number: Mapped[int]
+    name: Mapped[str] = mapped_column(String(100))
     description: Mapped[str] = mapped_column(String(500))
+
+    __table_args__ = (
+        UniqueConstraint("number", name="UQ_number"),
+        UniqueConstraint("name", name="UQ_name"),
+    )
 
     def __repr__(self) -> str:
         return f"{self.id} -- {self.number} - {self.name}\n"
