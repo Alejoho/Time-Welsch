446367eb8b3ac0c4ea2a155ab3059607a5509f28
Alejoho <alejoreilly96@gmail.com>
2024-12-20 08:00:18 -0500

    Add context to work with the shell

--- run.py
+++ run.py
@@ -1,6 +1,18 @@
 from app import create_app
 
+
 app = create_app()
+
+
+@app.shell_context_processor
+def make_shell_context():
+    from app import db
+    import sqlalchemy as sa
+    import sqlalchemy.orm as so
+    from app.models import User
+
+    return {"sa": sa, "so": so, "db": db, "User": User}
+
 
 if __name__ == "__main__":
     # app.run(debug=True, host="0.0.0.0", port=5000)
